7fbd0d14d0f8a6fde0187ef9aa162f1af65a0adf
yossef <yossefjo165@gmail.com>
2024-07-12 22:56:03 +0300

    sorry there is an bug and we will fix it in the future.

--- crud.js
+++ crud.js
@@ -39,6 +39,8 @@ let submit = document.getElementById('submit');
 let search = document.getElementById('search');
 let searchtitle = document.getElementById('searchtitle');
 let searchcategory = document.getElementById('searchcategory');
+let changer = 'create';
+let jo;
 
 // get total
 
@@ -63,7 +65,7 @@ if (localStorage.product != null){
     datapro =[];
 }
 
-submet.onclick =function() {
+submit.onclick =function() {
     let newpro = {
         title:title.value,
         price:price.value,
@@ -86,16 +88,85 @@ submet.onclick =function() {
     }else{
         datapro[jo] = newpro;
         changer = 'Create';
-        submet.innerHTML = 'Create';
+        submit.innerHTML = 'Create';
         count.style.display = 'block';
     }
-}
+
 
 // save it in the local storage
+
+localStorage.setItem('product',    JSON.stringify(datapro)); 
+cleardata();
+showdata();
+
+}
+
 // clear inputs
+
+function cleardata(){
+    title.value = '';
+    taxes.value = '';
+    price.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerHTML = '';
+    count.value = '';
+    category.value = '';
+}
+
 // read 
+
+function showdata(){
+    gettotal();
+let table_ = '';
+for (let i = 0; i < datapro.length; i++ ){
+    table_ += `
+    <tr>
+        <td>${i}</td>
+        <td>${datapro[i].title}</td>
+        <td>${datapro[i].price}</td>
+        <td>${datapro[i].taxes}</td>
+        <td>${datapro[i].ads}</td>
+        <td>${datapro[i].discount}</td>
+        <td>${datapro[i].total}</td>
+        <td>${datapro[i].category}</td>
+        <td><button onclick='updatedata(${i})' id="update_btn">Update</button></td>
+        <td><button onclick='deletedata(${i})' id="delete_btn">Delete</button></td>
+    </tr>
+
+`
+
+}
+
+    document.getElementById('tbody').innerHTML = table_ ;
+    let btndelete = document.getElementById('deleteall');
+    if (datapro.length > 0){
+        btndelete.innerHTML = `
+        <button onclick='deleteall()'>Delete All</button>
+        `
+    }else{
+        btndelete.innerHTML = ``;
+    }
+
+}
+
+
 // count 
+
+
+
 // delete (delete all) 
+function deletedata(i){
+    datapro.splice(i, 1);
+    localStorage.product = JSON.stringify(datapro);
+    showdata()
+}
+
+function deleteall(){
+    localStorage.clear()
+    datapro.splice(0)
+    showdata()
+}
 // update 
 // search 
 // clean data 
